1c104320957cb2b3e78c4a16a31739b832dbfe01
Pavel Buryka <l1kaf@yandex.ru>
2024-06-05 21:19:38 +0300

    add datadog

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,13 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/datadog/datadog" {
+  version = "3.39.0"
+  hashes = [
+    "h1:RaEi3hKTN9xVcv/Y0IhDZnB+88JQ/ugyFA9umNIkf/M=",
+  ]
+}
+
 provider "registry.terraform.io/yandex-cloud/yandex" {
   version = "0.121.0"
   hashes = [
--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
+    datadog = {
+      source = "DataDog/datadog"
+    }
   }
   required_version = ">= 0.13"
 }
@@ -15,30 +18,68 @@ variable "yc_token" {}
 provider "yandex" {
   zone = "ru-central1-a"
   token = var.yc_token
+}
+
+
+resource "yandex_compute_instance" "default" {
+  name        = "test"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+  folder_id   = "b1gmglvp72lqiuq0tobc"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.default.id
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "default" {
   folder_id = "b1gmglvp72lqiuq0tobc"
 }
 
-resource "yandex_vpc_network" "foo" {}
-
-resource "yandex_dns_zone" "zone1" {
-  name        = "my-private-zone"
-  description = "desc"
-
-  labels = {
-    label1 = "label-1-value"
-  }
-
-  zone             = "example.com."
-  public           = false
-  private_networks = [yandex_vpc_network.foo.id]
-
-  deletion_protection = false
+resource "yandex_vpc_subnet" "default" {
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.default.id}"
+  v4_cidr_blocks = ["10.5.0.0/24"]
+  folder_id      = "b1gmglvp72lqiuq0tobc"
 }
 
-resource "yandex_dns_recordset" "rs1" {
-  zone_id = yandex_dns_zone.zone1.id
-  name    = "srv.example.com."
-  type    = "A"
-  ttl     = 200
-  data    = ["10.1.0.1"]
+resource "yandex_compute_disk" "default" {
+  name     = "disk-name"
+  type     = "network-ssd"
+  zone     = "ru-central1-a"
+  image_id = "fd83s8u085j3mq231ago" // идентификатор образа Ubuntu
+  folder_id = "b1gmglvp72lqiuq0tobc"
+
+  labels = {
+    environment = "test"
+  }
+}
+
+variable "datadog_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "datadog_app_key" {
+  type      = string
+  sensitive = true
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://api.datadoghq.eu/"
 }
