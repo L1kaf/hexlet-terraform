4b64cf05bd60bcc480376a49fc5f95a634d395d2
Pavel Buryka <l1kaf@yandex.ru>
2024-06-06 20:50:51 +0300

    add load balancer

--- main.tf
+++ main.tf
@@ -13,60 +13,123 @@ terraform {
 // Terraform должен знать ключ, для выполнения команд по API
 
 // Определение переменной, которую нужно будет задать
-variable "yc_token" {}
+variable "yc_token" {
+  type = string
+  sensitive = true
+}
+
+variable "folder_id" {
+  type = string
+  sensitive = true
+}
+
+variable "yc_account_id" {
+  type = string
+  sensitive = true
+}
 
 provider "yandex" {
   zone = "ru-central1-a"
   token = var.yc_token
+  folder_id = var.folder_id
 }
 
 
-resource "yandex_compute_instance" "default" {
-  name        = "test"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-  folder_id   = "b1gmglvp72lqiuq0tobc"
+resource "yandex_compute_instance_group" "yandex-student-instance-group" {
+  name = "nlb-vm-group"
+  service_account_id = var.yc_account_id
 
-  resources {
-    cores  = 2
-    memory = 4
+  instance_template {
+    platform_id = "standard-v1"
+
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+        image_id = "fd8bkgba66kkf9eenpkb"
+        type     = "network-hdd"
+        size     = 8
+      }
+    }
+
+    network_interface {
+      network_id = yandex_vpc_network.network-1.id
+      subnet_ids = [yandex_vpc_subnet.subnet-1.id, yandex_vpc_subnet.subnet-2.id]
+      nat        = true
+    }
+
+    resources {
+      core_fraction = 5
+      cores         = 2
+      memory        = 2
+    }
+
+    # прерываемая
+    scheduling_policy {
+      preemptible = true
+    }
+
+
   }
 
-  boot_disk {
-    disk_id = yandex_compute_disk.default.id
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
   }
 
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.default.id}"
+  allocation_policy {
+    zones = ["ru-central1-a"]
   }
 
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 0
+  }
+
+  load_balancer {
+    target_group_name = "yandex-student-target-group"
   }
 }
 
-resource "yandex_vpc_network" "default" {
-  folder_id = "b1gmglvp72lqiuq0tobc"
+
+resource "yandex_vpc_network" "network-1" {
+  name = "yandex-student-network"
 }
 
-resource "yandex_vpc_subnet" "default" {
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "yandex-student-subnet-1"
   zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.default.id}"
-  v4_cidr_blocks = ["10.5.0.0/24"]
-  folder_id      = "b1gmglvp72lqiuq0tobc"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.1.0/24"]
 }
 
-resource "yandex_compute_disk" "default" {
-  name     = "disk-name"
-  type     = "network-ssd"
-  zone     = "ru-central1-a"
-  image_id = "fd83s8u085j3mq231ago" // идентификатор образа Ubuntu
-  folder_id = "b1gmglvp72lqiuq0tobc"
+resource "yandex_vpc_subnet" "subnet-2" {
+  name           = "yandex-student-subnet-2"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.2.0/24"]
+}
 
-  labels = {
-    environment = "test"
+resource "yandex_lb_network_load_balancer" "balancer" {
+  name = "balanser-test"
+  listener {
+    name        = "yandex-student-listener"
+    port        = 80
+    target_port = 80
+  }
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.yandex-student-instance-group.load_balancer.0.target_group_id
+    healthcheck {
+      name                = "health-check-1"
+      unhealthy_threshold = 5
+      healthy_threshold   = 5
+      http_options {
+        port = 80
+      }
+    }
   }
 }
+
 
 variable "datadog_api_key" {
   type      = string
